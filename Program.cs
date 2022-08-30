d3d82feb4c6bcace39184443c030f48fc7ce7495
denis.babich.acc@gmail.com <denis.babich.acc@gmail.com>
2022-08-30 21:41:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms
+{
+    internal sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
