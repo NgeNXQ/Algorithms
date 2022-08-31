d2f3f0a1586c7ccf0630adf9fd68d9f6ec04dde3
denis.babich.acc@gmail.com <denis.babich.acc@gmail.com>
2022-08-31 23:27:11 +0300

    BinarySearch added

---
+++ SearchingAlgorithms.cs
@@ -0,0 +1,59 @@
+﻿namespace Algorithms
+{
+    internal static class SearchingAlgorithms
+    {
+        public static int BinarySearch(int[] array, int value)
+        {
+            if (array == null)
+                return -1;
+
+            int low = 0;
+            int high = array.Length - 1;
+            int mid;
+
+            int guess;
+
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                guess = array[mid];
+
+                if (guess == value)
+                    return mid;
+                else if (guess < value)
+                    low = mid + 1;
+                else 
+                    high = mid - 1;
+            }
+
+            return -1;
+        }
+
+        public static int BinarySearch(char[] array, char value)
+        {
+            if (array == null)
+                return -1;
+
+            int low = 0;
+            int high = array.Length - 1;
+            int mid;
+
+            int guess;
+
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                guess = array[mid];
+
+                if (guess == value)
+                    return mid;
+                else if (guess < value)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+
+            return -1;
+        }
+    }
+}
